8290e0b1ef03d88b22fa2fc2b1a14234318fd470
mario <mario.uriarte@gmail.com>
2013-05-09 21:16:55 -0400

    inicio de proyecto

---
+++ apps/frontend/modules/categoria/lib/categoriaGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * categoria module configuration.
+ *
+ * @package    jobeet
+ * @subpackage categoria
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class categoriaGeneratorConfiguration extends BaseCategoriaGeneratorConfiguration
+{
+}
